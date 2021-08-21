ec3c9842a619d91caa54f15bf6aaec03c4a84259
cwghb <cwwei6789@163.com>
2021-08-21 14:00:19 +0800

    封装swiper组件

---
+++ pages/home/components/cc-swiper/cc-swiper.vue
@@ -0,0 +1,62 @@
+/*
+ * @Author: chenwei
+ * @Date: 2021-08-21 13:11:31
+ * @Last Modified by: chenwei
+ * @Last Modified time: 2021-08-21 13:48:04
+ swiper组件
+ */
+<template>
+    <view class='v_root posi_rel'>
+        <swiper :current="current" @change="onChange" circular="true" autoplay="true" :interval="3000" :duration="500">
+            <swiper-item class="border_r_16 " v-for="(dataItem,dIdx) in dataArr" :key="dIdx">
+                <image mode="heightFix" :src='dataItem.src' />
+            </swiper-item>
+        </swiper>
+        <view class='v_dots flex_c_row'>
+            <view v-for="(dataItem,idx) in dataArr" :key="idx" class='v_dot' :class="[idx==current?'v_sel_dot':'v_un_dot']"></view>
+        </view>
+    </view>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            current: 0,
+        };
+    },
+    props: {
+        dataArr: {
+            type: Array,
+            defaule: []
+        }
+    },
+    methods: {
+        onChange(e) {
+            this.current = e.detail.current;
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.v_dots {
+    position: absolute;
+    bottom: 20rpx;
+    right: 40rpx;
+}
+
+.v_dot {
+    width: 20rpx;
+    height: 20rpx;
+    border-radius: 10rpx;
+    background-color: violet;
+    margin-right: 10rpx;
+}
+.v_sel_dot {
+    background-color: #ffffff;
+}
+.v_un_dot {
+    background-color: rgba(255, 255, 255, 0.5);
+}
+</style>
--- pages/home/home.vue
+++ pages/home/home.vue
@@ -1,19 +1,64 @@
 <template>
-	<view>
-		home
-	</view>
+    <view>
+        <!-- #ifdef MP -->
+        <view class='v_nav'>导航栏</view>
+        <!-- #endif -->
+
+        <view class='v_wrap_swiper'>
+            <ccSwiper :dataArr="swipArr"></ccSwiper>
+        </view>
+
+        <view class=''>home</view>
+
+    </view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				
-			};
-		}
-	}
+import ccSwiper from './components/cc-swiper/cc-swiper.vue'
+export default {
+    data() {
+        return {
+            swipArr: [
+                {
+                    src: "/static/image/swiper_0.png",
+                    title: "汉服设计上线了"
+                },
+                {
+                    src: "/static/image/swiper_1.png",
+                    title: "汉服设计上线了"
+                },
+                {
+                    src: "/static/image/swiper_2.png",
+                    title: "汉服设计上线了"
+                }
+
+            ]
+
+
+        };
+    },
+
+    components: {
+        ccSwiper
+    },
+
+    onNavigationBarButtonTap() {
+        console.log('---签到=');
+    },
+
+    onNavigationBarSearchInputClicked() {
+        console.log('---home=search');
+    }
+}
 </script>
 
 <style lang="scss" scoped>
-
+.v_nav {
+    height: 88rpx;
+}
+.v_wrap_swiper {
+    margin-left: 20rpx;
+    margin-right: 20rpx;
+    height: 300rpx;
+}
 </style>
